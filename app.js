48f10b1dd35bb4709b5ef1ddec61cba4c732ac58
francheez <francheeze@proton.me>
2025-02-10 17:03:36 -0500

    Update

--- app.js
+++ app.js
@@ -33,8 +33,7 @@ map = {
     'Ю': 'Ju', 'ю': 'ju',
     'Я': 'Ja', 'я': 'ja',
     '\'': '\0', 'ʼ': '\0', 
-    '`': '\0', '´': '\0', 
-    '’': '\0'
+    '`': '\0', '´': '\0'
 }
 
 softLetters = {
@@ -65,31 +64,36 @@ function softening(input) {
             output += input[i];
         }     
     }    
-    
+  
     return output;
 }    
 
 function jotation(input) {
     let output = '';
 
-    const exceptionChars = "АЕІОУЯЄЇЮИаеіоуяєїюьи'ʼ\0\n\"«»=×^£¢€¥%\(\)\{\}\[\]&-—–·:;?!*\\/@#№|§∆+1234567890´`’ ";
-    const jottedChars = "яєїю";
+    const consonants = "бвгґдйклмнпрстфхцчшщьБВГҐДЙКЛМНПРСТФХЦЧШЩЬ";
+    const jottedVowels = {
+        "я": "ia",
+        "є": "ie",
+        "ю": "iu"
+    };
+    const tremaVowels = {
+        "а": "ä",
+        "е": "ë",
+        "о": "ö",
+        "у": "ü"
+    };
 
     for(let i = 0; i < input.length; i++) {  
-        // Handle i + vowel to avoid it being read as a softened consonants
-        if(input[i] == 'і' && "аеіоуи".includes(input[i + 1])) {
-            output += 'ij';
-        }   
-        // Turn jotted vowels (я, є, ю ...) to their soft version
-        else if(input[i] == 'я' && !exceptionChars.includes(input[i - 1]) && i != 0) {
-            output += 'ia';
-        }   
-        else if(input[i] == 'є' && !exceptionChars.includes(input[i - 1]) && i != 0) {
-            output += 'ie';
-        }    
-        else if(input[i] == 'ю' && !exceptionChars.includes(input[i - 1]) && i != 0) {
-            output += 'iu';
-        }    
+        if(tremaVowels[input[i]] && input[i -1] == 'і') { //Separate already existing combination of i + another vowel using trema 
+            output += tremaVowels[input[i]];
+        }
+        else if(input[i] == 'ь' && input[i + 1] == 'о') { //If there's a ь before an o
+            output += 'i';
+        }
+        else if(jottedVowels[input[i]] && consonants.includes(input[i - 1])) { //If current letter is a jotted vowel after a consonant
+            output += jottedVowels[input[i]]
+        }
         else {
             output += input[i]
         }
@@ -119,16 +123,29 @@ function convert(input) {
     return output;
 }
 
+let mode = "cyr-to-lat";
+
 const inputField = document.getElementById('input');
 const outputField = document.getElementById('output');
 const convertBtn = document.getElementById('convert');
 const copyBtn = document.getElementById('copy');
 const clearBtn = document.getElementById('clear');
+const modeBtn = document.getElementById('convert-mode-switch');
 
 function convertInput() {
     outputField.innerHTML = '';
     convertedText = convert(input.value);
     outputField.innerHTML = convert(input.value);
+}
+
+modeBtn.onclick = () => {
+    if(mode == "cyr-to-lat") {
+        mode = "lat-to-cyr";
+        modeBtn.innerHTML = "Lat->Кир";
+    } else {
+        mode = "cyr-to-lat";
+        modeBtn.innerHTML = "Кир->Lat";
+    }
 }
 
 convertBtn.onclick = () => {
@@ -141,25 +158,18 @@ document.addEventListener('keydown', function(event) {
     }
 });
 
-
 copyBtn.onclick = () => {
-    // Get the text content of the output field
     const textToCopy = outputField.innerText;
 
-    // Check if there's any text to copy
     if (textToCopy) {
-        // Use the Clipboard API to copy the text
         navigator.clipboard.writeText(textToCopy)
             .then(() => {
-                // Provide feedback that the text was copied
                 alert('Text copied to clipboard!');
             })
             .catch((err) => {
-                // Handle any errors
                 console.error('Failed to copy text: ', err);
             });
     } else {
-        // If there's no text to copy, alert the user
         alert('No text to copy!');
     }
 }
